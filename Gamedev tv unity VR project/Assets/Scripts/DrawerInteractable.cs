f5cfdbf9d305c494a6b331a62dc8b1e3cb6f8e4a
Vikram <thepractitioner.gg@gmail.com>
2024-06-10 15:52:33 +1000

    Inheritance, Drawer Socket Modified
    In this commit, we inherited drawerInteractble script from xr grab interactor and made it not grabbable when its locked by changing layers in the script by overriding the base functions

--- Gamedev tv unity VR project/Assets/Scripts/DrawerInteractable.cs
+++ Gamedev tv unity VR project/Assets/Scripts/DrawerInteractable.cs
@@ -4,10 +4,14 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.XR.Interaction.Toolkit;
 
-public class DrawerInteractable : MonoBehaviour
+public class DrawerInteractable : XRGrabInteractable
 {
     [SerializeField] XRSocketInteractor keySocket;
     [SerializeField] bool isLocked;
+
+    private Transform parentTransform;
+    private const string defaultLayer = "Default";
+    private const string grabLayer = "Grab";
     void Start()
     {
         if (keySocket != null)
@@ -15,6 +19,7 @@ public class DrawerInteractable : MonoBehaviour
             keySocket.selectEntered.AddListener(OnDrawerUnlocked);
             keySocket.selectExited.AddListener(OnDrawerLocked);
         }
+        parentTransform = transform.parent.transform;
     }
 
     private void OnDrawerLocked(SelectExitEventArgs arg0)
@@ -27,6 +32,23 @@ public class DrawerInteractable : MonoBehaviour
         isLocked = false;
     }
 
+    protected override void OnSelectEntered(SelectEnterEventArgs args)
+    {
+        if (isLocked)
+        {
+            transform.SetParent(parentTransform);
+        }
+        else
+        {
+            interactionLayers = InteractionLayerMask.GetMask(defaultLayer);
+        }
+    }
+
+    protected override void OnSelectExited(SelectExitEventArgs args)
+    {
+        base.OnSelectExited(args);
+        interactionLayers = InteractionLayerMask.GetMask(grabLayer);
+    }
     // Update is called once per frame
     void Update()
     {
